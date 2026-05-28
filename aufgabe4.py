e2fc1abddfa44e11d91ff96bda82950107c861f6
shackl <shackl@student.tgm.ac.at>
2026-05-28 09:28:39 +0200

    Abgabe Begründung

---
+++ aufgabe4.py
@@ -0,0 +1,6 @@
+# Reflexion,shackl:
+# Ich würde Streamlit für ein schnelles Datenanalyse-Dashboard verwenden,
+# weil man interaktive Apps mit wenig Code erstellen kann.
+# Außerdem ist die Lernkurve niedrig und erleichtert den EInstieg beim Erstellen des Codes.
+# Django oder FastAPI würde ich für größere Webanwendungen oder APIs verwenden, 
+# da sie besser für Benutzerverwaltung, Datenbanken und skalierbare Backend-Systeme geeignet sind.
